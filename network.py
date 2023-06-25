bad399a186c8f2cbdb8ebe921e05d5140691586c
ehgus8 <ehgus8@kyonggi.ac.kr>
2023-06-25 09:13:34 +0900

    modified network structure

--- network.py
+++ network.py
@@ -5,46 +5,69 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 class ActorCritic(nn.Module):
-    def __init__(self, in_channels, policy_out):
+    def __init__(self, in_channels, policy_out, game_size):
         super(ActorCritic, self).__init__()
         self.epoch = 0
-        self.conv1 = nn.Conv2d(in_channels, out_channels=8, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(8, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(32, out_channels=16, kernel_size=3, stride=1, padding=1)
-        self.pi = nn.Linear(16*5*5, policy_out)
-        self.v = nn.Linear(16*5*5, 1)
+        self.conv1 = nn.Conv2d(in_channels, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(128)
+        self.conv2 = nn.Conv2d(128, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(128, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.pi = nn.Linear(64*game_size*game_size, policy_out)
+        self.v1 = nn.Linear(64*game_size*game_size, policy_out)
+        self.v2 = nn.Linear(policy_out, 1)
 
-    def policy(self, x):
-        x = F.relu(self.conv1(x))
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(self.bn1(x))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
         x = torch.flatten(x, 1)
+        pi = self.policy(x)
+        v = self.value(x)
+        
+        return pi, v
+
+    def policy(self, x):
+        # x = self.conv1(x)
+        # x = F.relu(self.bn1(x))
+        # x = F.relu(self.conv2(x))
+        # x = F.relu(self.conv3(x))
+        # x = torch.flatten(x, 1)
         pi = self.pi(x)
         prob = F.softmax(pi,1)
         return pi, prob
 
     def value(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = torch.flatten(x, 1)
-        v = F.tanh(self.v(x))
+        # x = self.conv1(x)
+        # x = F.relu(self.bn1(x))
+        # x = F.relu(self.conv2(x))
+        # x = F.relu(self.conv3(x))
+        # x = torch.flatten(x, 1)
+        v = self.v1(x)
+        v = F.tanh(self.v2(v))
         return v
 
-    def train_net(self, memory, optimizer, batch_size):
+    def train_net(self, memory, optimizer, batch_size, epoch = 1):
         iter = memory.size()//batch_size + 1
-        epoch_loss = 0
-        for i in range(iter):
-            s, pi, r = memory.sample(batch_size)
-            # print(s.shape, pi.shape, r.shape)
-            # print(self.value(s), self.value(s).shape)
-            loss =  F.mse_loss(self.value(s), r) + F.cross_entropy(self.policy(s)[0], pi)
+        for epoch in range(epoch):
+            epoch_loss = 0
+            epoch_val_loss = 0
+            epoch_policy_loss = 0
+            for i in range(iter):
+                s, pi, r = memory.sample(batch_size)
+                # print(s.shape, pi.shape, r.shape)
+                # print(self.value(s), self.value(s).shape)
+                pi_pred, v_pred = self(s)
+                val_loss = F.mse_loss(v_pred, r)
+                policy_loss = F.cross_entropy(pi_pred[0], pi)
+                loss = val_loss + policy_loss
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
-            epoch_loss += s.shape[0] * loss.item()
-
-        # print epoch loss
-        print(f"epoch :{self.epoch + 1}, loss :{epoch_loss / memory.size()}")
+                epoch_val_loss += s.shape[0] * val_loss.item()
+                epoch_policy_loss += s.shape[0] * policy_loss.item()
+                epoch_loss += s.shape[0] * loss.item()
+            # print epoch loss
+            print(f"epoch :{epoch + 1}, loss :{epoch_loss / memory.size()}, val_loss :{epoch_val_loss / memory.size()}, policy_loss :{epoch_policy_loss / memory.size()}")
