3469a8eb217c87d9a949d1e174a9667c09b277fa
ehgus8 <ehgus8@kyonggi.ac.kr>
2023-06-25 22:20:38 +0900

    fixed

--- play_with_agent.py
+++ play_with_agent.py
@@ -10,9 +10,11 @@ def play(model, env, mcts, turn, sample_method = 'mcts_distribution'):
     root_node.s = torch.from_numpy(env.get_state()).float().detach()
 
     a, pi, node = mcts.sample_action(root_node=root_node, 
-                                iteration = 100,
+                                iteration = 400,
                                 model = model,
                                 sample_method=sample_method)
+    
+    print(pi)
     s_prime, r, done = env.step(a)
 
     if done == True:
@@ -33,20 +35,23 @@ def main():
     s, _ = env.reset()
     done = False
 
-    turn = int(input("Select turn 1: you first, -1: agent first"))
+    human_turn = int(input("Select turn 1: you first, -1: agent first"))
 
-    
         
+    turn = 1
     while not done:
-        if turn == 1:
+        if turn == human_turn:
             env.display_board()
             action = list(map(int,input("coordinate(y x): ").split()))
             action = action[0] * game_size + action[1]
             s_prime, r, done = env.step(action=action)
+            turn = turn * -1
         if done:
             break
 
         done = play(model, env, mcts, turn, sample_method='mcts_distribution')
+        turn = turn * -1
+    env.display_board()
 
 if __name__ == '__main__':
     main()
